d1cffd90b5ecae553c6f80ce7a94659567a30d21
lavkush9051 <lavkush9051@gmail.com>
2025-09-26 12:32:25 +0530

    test: pwa added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+// }
+
+// export default nextConfig
+
+import withPWA from "next-pwa"
+ 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -10,5 +27,8 @@ const nextConfig = {
     unoptimized: true,
   },
 }
-
-export default nextConfig
+ 
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig)
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,6 +63,8 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="AttendEase" /> */}
 
+        <link rel="manifest" href="/manifest.json" />
+
         <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
